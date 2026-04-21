f8ef0461f18e44b32cfb7d077f6da5a8a254ec53
auto1225 <62019533+auto1225@users.noreply.github.com>
2026-04-21 18:42:35 +0900

    feat(cms): wrap app in ContentProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './auth/AuthContext';
+import { ContentProvider } from './content/ContentContext';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ContentProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ContentProvider>
   </React.StrictMode>
 );
